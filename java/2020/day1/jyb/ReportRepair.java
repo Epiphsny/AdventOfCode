a218c156695e613460ee2f72217003d0e092565f
jbabine1 <90419886+jbabine1@users.noreply.github.com>
2025-03-12 03:48:34 -0300

    Completed Day1-2020 and Day1-2022

---
+++ java/2020/day1/jyb/ReportRepair.java
@@ -0,0 +1,42 @@
+package day1.jyb;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ReportRepair {
+
+	public static void main(String[] args) {
+
+		ArrayList<Integer> list = new ArrayList<>();
+		int answer = 0;
+		
+		try {
+			File data = new File("C:\\SWE\\AdventOfCode\\resources\\2020\\day1_input.txt");
+		    Scanner reader = new Scanner(data);
+		    while (reader.hasNextLine()) {
+		    	String line = reader.nextLine();
+		        list.add(Integer.parseInt(line));
+		    }
+		    reader.close();
+		} 
+		catch (FileNotFoundException e) {
+		      System.out.println("An error occurred.");
+		}
+		
+		for (int numberBase: list) {
+			for (int numberCompare1: list) {
+				for (int numberCompare2: list) {
+					if ((numberBase + numberCompare1 + numberCompare2) == 2020) {
+						answer = numberBase * numberCompare1 * numberCompare2;
+					}
+				}
+			}
+		}
+		
+		System.out.println("Answer: " + answer);
+
+	}
+
+}
---
+++ java/2022/day1/jyb/CalorieCounting.java
@@ -0,0 +1,54 @@
+package day1.jyb;
+
+import java.io.File; 
+import java.io.FileNotFoundException; 
+import java.util.Scanner;
+
+// Author Justin Babineau jbabine1
+public class CalorieCounting {
+
+	public static void main(String[] args) {
+		
+		int topCal1 = 0;
+		int topCal2 = 0;
+		int topCal3 = 0;
+		int curCal = 0;
+		
+		try {
+			File data = new File("C:\\SWE\\AdventOfCode\\resources\\2022\\day1_input.txt");
+		    Scanner reader = new Scanner(data);
+		    while (reader.hasNextLine()) {
+		    	String line = reader.nextLine();
+		        if (line.compareTo("") == 0) {
+		        	if (curCal > topCal1) {
+		        		topCal3 = topCal2;
+		        		topCal2 = topCal1;
+		        		topCal1 = curCal;
+		        	}
+		        	else if (curCal > topCal2) {
+		        		topCal3 = topCal2;
+		        		topCal2 =  curCal;
+		        	}
+		        	else if (curCal > topCal3) {
+		        		topCal3 =  curCal;
+		        	}
+		        	curCal = 0;
+		        }
+		        else {
+		        	curCal += Integer.parseInt(line);
+		        }
+		    }
+		    reader.close();
+		} 
+		catch (FileNotFoundException e) {
+		      System.out.println("An error occurred.");
+		}
+		
+		int sumTopCal = topCal1 + topCal2 + topCal3;
+		
+		System.out.println("Max Calories: " + topCal1);
+		System.out.println("Sum Top Calories: " + sumTopCal);
+		
+	}
+
+}
