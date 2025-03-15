11acad2b1dfc961c4a89387526c5ec380e708352
jbabine1 <90419886+jbabine1@users.noreply.github.com>
2025-03-15 10:47:51 -0300

    Completed Part1 of Day1-2023

--- java/2023/day1/jyb/Trebuchet.java
+++ java/2023/day1/jyb/Trebuchet.java
@@ -8,30 +8,63 @@ public class Trebuchet {
     public static void main(String[] args) {
 
         int sum = 0;
-        int digit1;
-        int digit2;
-        boolean foundDigit1;
+        int Firstdigit;
+        int Lastdigit;
+        boolean foundFirstDigit;
 
         try {
-            File data = new File("C:\\SWE\\AdventOfCode\\resources\\2024\\day1_input.txt");
+            File data = new File("C:\\SWE\\AdventOfCode\\resources\\2023\\day1_input.txt");
             Scanner reader = new Scanner(data);
             while (reader.hasNextLine()) {
             	String line = reader.nextLine();
-                foundDigit1 = false;
-                digit1 = 0;
-                digit2 = 0;
+                foundFirstDigit = false;
+                Firstdigit = 0;
+                Lastdigit = 0;
                 for (int i = 0; i < line.length(); i++) {
                     if (Character.isDigit(line.charAt(i))) {
-                        if (!foundDigit1) {
-                            digit1 = Character.getNumericValue(line.charAt(i));
-                            foundDigit1 = true;
+                        if (!foundFirstDigit) {
+                            Firstdigit = Character.getNumericValue(line.charAt(i));
+                            Lastdigit = Firstdigit;
+                            foundFirstDigit = true;
                         }
                         else {
-                            digit2 = Character.getNumericValue(line.charAt(i));
+                            Lastdigit = Character.getNumericValue(line.charAt(i));
                         }
                     }
                 }
-                sum += digit1 + digit2;
+                sum += ((Firstdigit * 10) + Lastdigit);
+            }
+            reader.close();
+        } 
+        catch (FileNotFoundException e) {
+              System.out.println("An error occurred.");
+        }
+
+        System.out.println("Sum: " + sum);
+
+        sum = 0;
+        // Part 2 not finished
+        try {
+            File data = new File("C:\\SWE\\AdventOfCode\\resources\\2023\\day1_input.txt");
+            Scanner reader = new Scanner(data);
+            while (reader.hasNextLine()) {
+            	String line = reader.nextLine();
+                foundFirstDigit = false;
+                Firstdigit = 0;
+                Lastdigit = 0;
+                for (int i = 0; i < line.length(); i++) {
+                    if (Character.isDigit(line.charAt(i))) {
+                        if (!foundFirstDigit) {
+                            Firstdigit = Character.getNumericValue(line.charAt(i));
+                            Lastdigit = Firstdigit;
+                            foundFirstDigit = true;
+                        }
+                        else {
+                            Lastdigit = Character.getNumericValue(line.charAt(i));
+                        }
+                    }
+                }
+                sum += ((Firstdigit * 10) + Lastdigit);
             }
             reader.close();
         } 
