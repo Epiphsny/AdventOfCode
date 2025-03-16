db3e381766182cb2c9540f70628dd85632d6bd5f
jbabine1 <jusbabgamer@gmail.com>
2025-03-16 00:57:05 -0300

    Continuing Day1-2018

--- java/2018/day1/jyb/ChronalCalibration.java
+++ java/2018/day1/jyb/ChronalCalibration.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 // Author Justin Babineau Epiphsny
@@ -7,7 +8,11 @@ public class ChronalCalibration {
     
     public static void main(String[] args) {
 
+        ArrayList<String> frequencyChanges = new ArrayList<String>();
+        ArrayList<Integer> previousFrequencies = new ArrayList<Integer>();
         int frequency = 0;
+        int frequencyTwice = 0;
+        boolean found = false;
 
         try {
             File data = new File("C:\\SWE\\AdventOfCode\\resources\\2018\\day1_input.txt");
@@ -28,6 +33,53 @@ public class ChronalCalibration {
         }
 
         System.out.println("Final frequency: " + frequency);
+
+        frequency = 0;
+        try {
+            File data = new File("C:\\SWE\\AdventOfCode\\resources\\2018\\day1_input.txt");
+            Scanner reader = new Scanner(data);
+            while (reader.hasNextLine() && !found) {
+            	String line = reader.nextLine();
+                if (frequencyChanges.contains(line)) {
+                    for (int i = 0; i < frequencyChanges.size(); i++) {
+                        if (frequencyChanges.get(i).charAt(0) == '+') {
+                            frequency += Integer.parseInt(frequencyChanges.get(i).substring(1));
+                        }
+                        else {
+                            frequency -= Integer.parseInt(frequencyChanges.get(i).substring(1));
+                        } 
+                        if (previousFrequencies.contains(frequency)) {
+                            frequencyTwice = frequency;
+                            found = true;
+                            break;
+                        }
+                        previousFrequencies.add(frequency);
+                    }
+                }
+                if (found) {
+                    break;
+                }
+                frequencyChanges.add(line);
+                if (line.charAt(0) == '+') {
+                    frequency += Integer.parseInt(line.substring(1));
+                }
+                else {
+                    frequency -= Integer.parseInt(line.substring(1));
+                }
+                if (previousFrequencies.contains(frequency)) {
+                    frequencyTwice = frequency;
+                    found = true;
+                    break;
+                }
+                previousFrequencies.add(frequency);
+            }
+            reader.close();
+        } 
+        catch (FileNotFoundException e) {
+              System.out.println("An error occurred.");
+        }
+
+        System.out.println("Frequancy Found Twice: " + frequencyTwice);
 
     }
 
